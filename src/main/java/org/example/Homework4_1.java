c6465479235a64df15e57a695ac3b89efecfab7d
Katia <kateeegold+1@gmail.com>
2024-10-16 11:03:34 +0200

    Initial commit

---
+++ src/main/java/org/example/Homework4_1.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Homework4_1 {
+    void square(int arg) {
+        System.out.println(arg * arg);
+    }
+
+    public static void main(String[] args) {
+        Homework4_1 object = new Homework4_1();
+        object.square(4);
+    }
+}
