a318a46a7a8c4042c5531877c1d6d9d9e0f9d6b7
Katia <kateeegold+1@gmail.com>
2024-10-16 19:43:47 +0200

    Updating

--- src/main/java/org/example/Homework4_1.java
+++ src/main/java/org/example/Homework4_1.java
@@ -5,10 +5,22 @@ public class Homework4_1 {
 
         System.out.println("Введено число: " + arg);
         System.out.println("Квадрат вашего числа = " + arg*arg);
+        System.out.println();
     }
+    void cylinder(double radius, double height) {
+        System.out.printf("Объем цилиндра с %s радиусом и %s высотой = %s" ,radius, height, Math.PI*radius*radius*height);
+        System.out.println();
 
+    }
+    void power(int a, int b) {
+        System.out.println();
+        System.out.printf("Число %s в степени %s = %s ", a, b, Math.pow(a,b));
+        System.out.println();
+    }
     public static void main(String[] args) {
         Homework4_1 object = new Homework4_1();
         object.square(4);
+        object.cylinder(3.5,10 );
+        object.power(2,3);
     }
 }
